6d5d64c2b2eb9b63d9ba37fc74b80c31c72326aa
datsuns <the.skeleton7@gmail.com>
2025-08-07 22:57:33 +0900

    python sample: ignore error code on with_exec

--- hello-python/src/hello/main.py
+++ hello-python/src/hello/main.py
@@ -1,9 +1,16 @@
 import dagger
-from dagger import dag, function, object_type
+from dagger import dag, function, object_type, ReturnType
 
 
 @object_type
 class Hello:
+    @function
+    async def ignore_error_on_with_exec(self, string_arg: str) -> dagger.Container:
+        c = dag.container().from_("alpine:latest").with_exec(["exit", "1"], expect=ReturnType.ANY)
+        c = c.with_exec(["sh", "-c", "echo hello >tmp.txt"])
+        c.file("tmp.txt").export("log.txt")
+        return await c
+
     @function
     def container_echo(self, string_arg: str) -> dagger.Container:
         """Returns a container that echoes whatever string argument is provided"""
